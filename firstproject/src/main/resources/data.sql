1f16d48d97bf9c7ce320c83b7c5eda8d3a2346fd
jjin-21 <ljj1900@naver.com>
2024-02-19 15:11:26 +0900

    Changes

--- firstproject/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article(id, title, content) VALUES (1, 'aaaa', '1111')
-INSERT INTO article(id, title, content) VALUES (2, 'bbbb', '2222')
-INSERT INTO article(id, title, content) VALUES (3, 'cccc', '3333')
