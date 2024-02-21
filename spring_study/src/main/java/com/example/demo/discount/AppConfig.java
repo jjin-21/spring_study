5efc0f7461b8b36dec575bfeee975896e1d011c4
jjin-21 <ljj1900@naver.com>
2024-02-21 17:56:36 +0900

    240220

--- spring_study/src/main/java/com/example/demo/discount/AppConfig.java
+++ spring_study/src/main/java/com/example/demo/discount/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.discount;
 
+import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
@@ -12,11 +13,21 @@ import com.example.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+
     }
 
 
